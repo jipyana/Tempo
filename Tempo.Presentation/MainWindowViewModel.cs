2e01c80983e7a8710d6862fe80450424bd20c3ec
Miroslav Jelaska <jelaska.miro@gmail.com>
2014-10-02 04:49:44 +0200

    Initial code.

---
+++ Tempo.Presentation/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ent = Tempo.Main.Entities;
+
+namespace Tempo
+{
+    public class MainWindowViewModel : DependencyObject
+    {
+        public ObservableCollection<ent::Song> SongsList
+        {
+            get { return (ObservableCollection<ent::Song>)GetValue(ImagesListProperty); }
+            set { SetValue(ImagesListProperty, value); }
+        }
+        public static readonly DependencyProperty ImagesListProperty = DependencyProperty.Register("SongsList", typeof(ObservableCollection<ent::Song>), typeof(MainWindowViewModel), new UIPropertyMetadata());
+
+        public ent::Song SelectedSong
+        {
+            get { return (ent::Song) GetValue(SelectedSongProperty); }
+            set { SetValue(SelectedSongProperty, value); }
+        }
+        public static readonly DependencyProperty SelectedSongProperty = DependencyProperty.Register("SelectedSong", typeof (ent::Song), typeof (MainWindowViewModel), new PropertyMetadata());
+
+
+        public MainWindowViewModel()
+        {
+            this.SongsList = new ObservableCollection<ent.Song>();
+            this.SongsList.Add(new ent.Song("Name1", "c bb"));
+            this.SongsList.Add(new ent.Song("Name2", "d bb"));
+        }
+    }
+}
---
+++ Tempo/Main/Domain_Entities.cs
@@ -0,0 +1,17 @@
+﻿namespace Tempo.Main.Entities
+{
+    public class Song
+    {
+        public Song
+        (
+            string name,
+            string uri
+        )
+        {
+            Name = name;
+            Uri = uri;
+        }
+        public string Name { get; private set; }
+        public string Uri  { get; private set; }
+    }
+}
