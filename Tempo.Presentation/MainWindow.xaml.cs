67eb9f96b05b3af10b761d22983d8c46b1dabde5
Jam <jipyana@student.neumont.edu>
2018-08-15 16:25:15 -0600

    Tab Functionality is working

--- Tempo.Presentation/MainWindow.xaml.cs
+++ Tempo.Presentation/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using MahApps.Metro;
 using MahApps.Metro.Controls;
+using Tempo.Main.Entities;
+using Tempo.Presentation.ViewModel;
 
 namespace Tempo.Presentation
 {
@@ -12,9 +16,11 @@ namespace Tempo.Presentation
     {
         public MainWindow()
         {
+
             InitializeComponent();
             vm = new ViewModel.MainWindowViewModel();
             this.DataContext = vm;
+
         }
 
         private readonly ViewModel.MainWindowViewModel vm;
@@ -27,6 +33,8 @@ namespace Tempo.Presentation
         private void OpenSettingsWindow(object sender, RoutedEventArgs e)
         {
             var window = new SettingsWindow();
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            var songs = mainWindowViewModel.SongsList;
             window.ShowDialog();
         }
 
@@ -35,20 +43,50 @@ namespace Tempo.Presentation
             int index = int.Parse(((Button)e.Source).Uid);
 
             GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
+                   
 
             switch (index)
             {
                 case 0:
                     GridMain.Background = Brushes.Aquamarine;
+                    songList.Visibility = Visibility.Visible;
                     break;
                 case 1:
                     GridMain.Background = Brushes.Beige;
+                    
+                       
+                        songList.Visibility = Visibility.Hidden;
+                        
+
+                    
+                    //GridMain.IsVisible;
+
                     break;
                 case 2:
                     GridMain.Background = Brushes.CadetBlue;
+                    songList.Visibility = Visibility.Hidden;
+                    //clear_music_list();
                     break;
-              
+
             }
         }
+
+
+
+        //private void GridMain_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        //{
+        //    MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+        //    var songs = mainWindowViewModel.SongsList;
+
+        //    if (!((bool)e.NewValue))
+        //    {
+
+        //        mainWindowViewModel.SongsList.Clear();
+
+
+        //    }
+        //    // songList.Items.Refresh();
+
+        //}
     }
 }
