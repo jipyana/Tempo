9b0a13e7baeabd0d98d43e8b3e2c6b28886a8ade
Kaden Curtis <kdncrts@gmail.com>
2018-08-22 15:12:33 -0600

    Added Model Classes for HTTP requests

---
+++ Tempo/Models/Song.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tempo.Models
+{
+    public class Song
+    {
+
+        private int id;
+
+        private String title;
+
+        private String artist;
+
+        private String genre;
+
+        private String filePath;
+
+        private int hours;
+
+        private int minutes;
+
+        private int seconds;
+
+        private int fileSize;
+
+        public Song()
+        {
+
+        }
+
+        public Song(Song s)
+        {
+            this.id = s.id;
+            this.title = s.title;
+            this.artist = s.artist;
+            this.genre = s.genre;
+            this.filePath = s.filePath;
+            this.hours = s.hours;
+            this.minutes = s.minutes;
+            this.seconds = s.seconds;
+            this.fileSize = s.fileSize;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public String getTitle()
+        {
+            return title;
+        }
+
+        public void setTitle(String title)
+        {
+            this.title = title;
+        }
+
+        public String getArtist()
+        {
+            return artist;
+        }
+
+        public void setArtist(String artist)
+        {
+            this.artist = artist;
+        }
+
+        public String getGenre()
+        {
+            return genre;
+        }
+
+        public void setGenre(String genre)
+        {
+            this.genre = genre;
+        }
+
+        public int getHours()
+        {
+            return hours;
+        }
+
+        public void setHours(int hours)
+        {
+            this.hours = hours;
+        }
+
+        public int getMinutes()
+        {
+            return minutes;
+        }
+
+        public void setMinutes(int minutes)
+        {
+            this.minutes = minutes;
+        }
+
+        public int getSeconds()
+        {
+            return seconds;
+        }
+
+        public void setSeconds(int seconds)
+        {
+            this.seconds = seconds;
+        }
+
+        public int getFileSize()
+        {
+            return fileSize;
+        }
+
+        public void setFileSize(int fileSize)
+        {
+            this.fileSize = fileSize;
+        }
+
+        public String getFilePath()
+        {
+            return filePath;
+        }
+
+        public void setFilePath(String filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public String toString()
+        {
+            return "[Song: [id: " + id + "], [title: " + title + "], [artist: " + artist + "]]";
+        }
+    }
+
+}
---
+++ Tempo/Models/SongWithFileBytes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tempo.Models
+{
+    public class SongWithFileBytes
+    {
+
+        public Song song;
+
+        public byte[] fileBytes;
+
+        public SongWithFileBytes(Song s, byte[] bytes)
+        {
+            song = s;
+            fileBytes = bytes;
+        }
+    }
+
+}
