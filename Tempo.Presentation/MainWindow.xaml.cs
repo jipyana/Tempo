4c37b5c9afd96dc66e887d59de6b7e9f211f1d74
Jam <jipyana@student.neumont.edu>
2018-08-23 16:09:37 -0600

    /Tabs: gitflow-feature-stash: MusicSeeker

--- Tempo.Presentation/MainWindow.xaml.cs
+++ Tempo.Presentation/MainWindow.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 using MahApps.Metro;
 using MahApps.Metro.Controls;
 using Tempo.Main.Entities;
@@ -15,6 +17,8 @@ namespace Tempo.Presentation
 {
     public partial class MainWindow : MetroWindow
     {
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
 
         public MainWindow()
         {
@@ -23,8 +27,42 @@ namespace Tempo.Presentation
             vm = new ViewModel.MainWindowViewModel();
             this.DataContext = vm;
 
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            //timer.Tick += timer_Tick;
+            timer.Start();
+
         }
 
+        //private void timer_Tick(object sender, EventArgs e)
+        //{
+        //    if ((S.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+        //    {
+        //        sliProgress.Minimum = 0;
+        //        sliProgress.Maximum = mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
+        //        sliProgress.Value = mePlayer.Position.TotalSeconds;
+        //    }
+        //}
+        private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        //private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        //{
+        //    userIsDraggingSlider = false;
+        //    mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+        //}
+
+        private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+        }
+
+        //private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        //{
+        //    GridMain.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+        //}
         private readonly ViewModel.MainWindowViewModel vm;
 
         private void PlaylistElement_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -40,23 +78,6 @@ namespace Tempo.Presentation
             window.ShowDialog();
         }
 
-       
-
-
-        //private void GridMain_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        //{
-        //    MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
-        //    var songs = mainWindowViewModel.SongsList;
-
-        //    if (!((bool)e.NewValue))
-        //    {
-
-        //        mainWindowViewModel.SongsList.Clear();
-
-
-        //    }
-        //    // songList.Items.Refresh();
-
-        //}
+      
     }
 }
