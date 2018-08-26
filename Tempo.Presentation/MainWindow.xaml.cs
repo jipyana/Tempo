8c5a18d8c2677dfe835f756c4ef82b439ae3ccec
Kaden Curtis <kdncrts@gmail.com>
2018-08-26 16:44:04 -0600

    Finish Cloud library tab

--- Tempo.Presentation/MainWindow.xaml.cs
+++ Tempo.Presentation/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ using MahApps.Metro.Controls;
 using Tempo.Main.Entities;
 using Tempo.Presentation.UserControls;
 using Tempo.Presentation.ViewModel;
+using System.Windows.Documents;
+using System.Net;
+using System.IO;
 
 namespace Tempo.Presentation
 {
@@ -22,6 +25,8 @@ namespace Tempo.Presentation
             InitializeComponent();
             vm = new ViewModel.MainWindowViewModel();
             this.DataContext = vm;
+
+            // HTTP request first 100 songs
 
         }
 
@@ -40,7 +45,57 @@ namespace Tempo.Presentation
             window.ShowDialog();
         }
 
-       
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        // kaden.ghostsofutah.com:9578/music/help
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            String title = titleTextBox.Text;
+            String artist = artistTextBox.Text;
+            String genre = genreTextBox.Text;
+
+            //string json = string.Empty;
+            //string httpRequestString = $"kaden.ghostsofutah.com:9578/music/getSongs/title={title}&artist={artist}&genre={genre}";
+            //Console.WriteLine(httpRequestString);
+            ////HTTP request with search parameters
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(httpRequestString);
+            //request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            //using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            //using (Stream stream = response.GetResponseStream())
+            //using (StreamReader reader = new StreamReader(stream))
+            //{
+            //    json = reader.ReadToEnd();
+            //}
+
+            //// Convert JSON into ArrayList<Song>        
+            //List<Song> songs = ConvertSongsFromJSON(json);
+
+            //foreach (Song s in songs)
+            //{
+            //    TableRow tableRow = new TableRow();
+            //    tableRow.Cells.Add(new TableCell(new Paragraph(new Run("100 Letters"))));
+            //    tableRow.Cells.Add(new TableCell(new Paragraph(new Run("Halsey"))));
+            //    tableRow.Cells.Add(new TableCell(new Paragraph(new Run("Alternative"))));
+            //    tableRow.Cells.Add(new TableCell(new Paragraph(new Run("0:3:30"))));
+            //    tableRow.Cells.Add(new TableCell(new Paragraph(new Run("0"))));
+            //    cloudLibraryTable.RowGroups[0].Rows.Add(tableRow);
+            //}
+            // Put all of them into table
+
+
+        }
+
+        private List<Song> ConvertSongsFromJSON(string json)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Song>>(json);
+        }
+
+
 
 
         //private void GridMain_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
