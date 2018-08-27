cf40bf5be01fd6d274c584333ec652c2257fbb67
Kaden Curtis <kdncrts@gmail.com>
2018-08-26 21:09:37 -0600

    Add CloudModels directory and http get models

---
+++ Tempo/CloudModels/SongWithFileBytes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tempo.CloudModels
+{
+    class SongWithFileBytes
+    {
+        public Song song;
+
+        public byte[] fileBytes;
+
+        public SongWithFileBytes(Song s, byte[] bytes)
+        {
+            song = s;
+            fileBytes = bytes;
+        }
+    }
+}
